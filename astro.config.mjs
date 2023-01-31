5c3f447b212af4144b5d5da69d138fe1055f6a1f
lolgio <gioguccimane@gmail.com>
2023-01-31 13:48:47 +1300

    Projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    vite: {
-        ssr: {
-            external: ["svgo"],
-        },
-    },
 });
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Project = {
+    name: string;
+    description: string;
+    image: string;
+    link?: string;
+    github?: string;
+    tags: string[];
+};
